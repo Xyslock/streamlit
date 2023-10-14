116593685f398a96fb5480ea0a088f6095a1563a
negro <you@example.com>
2023-10-14 17:07:42 -0500

    actualizar

--- pages/3_limpeza_datos.py
+++ pages/3_limpeza_datos.py
@@ -1,23 +1,50 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import os
 
 
-ventas_vehiculos = st.file_uploader("Cargar archivo", type=["csv"])
-if ventas_vehiculos is not None:
-    df = pd.read_csv(ventas_vehiculos)
+ruta_csv = os.path.abspath('ventas_vehiculos .csv')
 
-    st.write("Valores faltantes:")
-    st.write(df.isnull().sum())
 
-    @st.experimental_memo
-    def convert_df(df):
-        return df.to_csv(index=False).encode('utf-8')
+if not os.path.isfile(ruta_csv):
+    st.error(f"No se encontró el archivo CSV en la ruta: {ruta_csv}")
+else:
+    
+    df = pd.read_csv(ruta_csv)
 
-    csv = convert_df(df)
-    st.download_button(
-        "Descargar datos limpios",
-        csv,
-        "datos_limpios.csv",
-        "text/csv",
-        key='download-csv'
-    )
+  
+    df = df.drop_duplicates()
+
+   
+    df = df.dropna()
+
+    filtro_opcion = st.selectbox("Selecciona una opción de filtro:", ['Nuevo/Usado', 'Color', 'Fecha', 'Vendido', 'Precio'])
+
+    if filtro_opcion == 'Nuevo/Usado':
+      
+        df = df.sort_values(by='Año', ascending=False)
+
+    elif filtro_opcion == 'Color':
+      
+        df = df.sort_values(by='Color')
+
+    elif filtro_opcion == 'Fecha':
+    
+        df['Fecha'] = pd.to_datetime(df['Fecha'])
+        df = df.sort_values(by='Fecha', ascending=False)
+
+    elif filtro_opcion == 'Vendido':
+
+        df = df.sort_values(by='Vendido', ascending=False)
+
+    elif filtro_opcion == 'Precio':
+    
+        df = df.sort_values(by='Precio', ascending=False)
+
+ 
+    df = df[df['Marca'].apply(lambda x: isinstance(x, str))]
+
+  
+    df.to_csv('ventas_vehiculos_filtrados.csv', index=False)
+    st.dataframe(df)
+    st.write(f"Registros restantes: {len(df)}")
